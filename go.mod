2dab4e150f8411d78233b5b057269273ca66542f
jtagcat <git-12dbd862@jtag.cat>
2021-10-29 07:33:51 +0300

    add pflag

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jtagcat/gazelle-torrent-from-file/v0
 
 go 1.16
+
+require github.com/spf13/pflag v1.0.5
--- main.go
+++ main.go
@@ -4,7 +4,41 @@ import (
 	"fmt"
 	"io/fs"
 	"path/filepath"
+
+	pflag "github.com/spf13/pflag"
 )
+
+// inspiration from restic
+var opts struct {
+	root_dir        string
+	torrentfile_dir string
+	apipath         string
+	apikey          string
+}
+
+func init() {
+	pflag.StringVarP(&opts.root_dir, "input", "i", "", "root directory, in what .torrent-less directories are in")
+	pflag.StringVarP(&opts.torrentfile_dir, "output", "o", "", "where .torrent files should be downloaded to")
+	pflag.StringVarP(&opts.apipath, "host", "h", "", "URL path to api, ex `https://foo.bar/ajax.php`")
+	pflag.StringVarP(&opts.apikey, "key", "k", "", "api key")
+	pflag.CommandLine.SortFlags = false
+	pflag.Parse()
+}
+
+func main() {
+	getDirs(opts.root_dir) //
+
+	// async for each dir:
+	//	// walk files
+	//	//	// api search with file
+	//	//	// if 0 hits, log error, brake
+	//	//	// if 1 hit, downloadTorrentFile(), brake
+
+	//	//	// if > hit, keep results, go to next file
+	//	//	// find common matches between last (carried set) and current
+	//	// if out of files, log error
+
+}
 
 // list directories inside root_dir
 func getDirs(root_dir string) []string {
@@ -23,21 +57,4 @@ func getDirs(root_dir string) []string {
 
 	dirs = dirs[1:] // 0th item would otherwise be root_dir
 	return dirs
-}
-
-func main() {
-	// flags: required: rootdir, apikey, apipath, torrentFileDir
-
-	// getDirs(FLAGHERE)
-
-	// async for each dir:
-	//	// walk files
-	//	//	// api search with file
-	//	//	// if 0 hits, log error, brake
-	//	//	// if 1 hit, downloadTorrentFile(), brake
-
-	//	//	// if > hit, keep results, go to next file
-	//	//	// find common matches between last (carried set) and current
-	//	// if out of files, log error
-
 }
